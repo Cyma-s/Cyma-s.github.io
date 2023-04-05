4d283ed2e6b25df27ce4adc1eed3289ab880853d
Cyma-s <ssol0319@naver.com>
2023-04-05 15:36:26 +0900

    setting: rss 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,5 +83,60 @@ module.exports = {
         types: ["Mdx"], // or ["MarkdownRemark"] (or both)
       },
     },
+	`gatsby-transformer-remark`,
+	{
+		resolve: `gatsby-plugin-feed`,
+		options: {
+      query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.edges.map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ 'content:encoded': edge.node.html }],
+              })
+            })
+          },
+          query: `
+            {
+              allMarkdownRemark(
+                sort: { fields: [frontmatter___date], order: DESC }
+                filter: { frontmatter: { draft: { eq: false } } },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+          output: '/rss.xml',
+		  title: "Vero Wiki RSS Feed",
+        },
+      ],
+    },	 
+	},
   ],
 };
