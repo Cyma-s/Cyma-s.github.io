1fb663da8961e78fa1f24899001f9c015df155d1
Cyma-s <ssol0319@naver.com>
2023-04-07 11:23:46 +0900

    setting: rss 설정 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,15 +15,15 @@ module.exports = {
     DEV_SSR: true,
   },
   plugins: [
-	{
-		resolve: `gatsby-plugin-google-gtag`,
-		options: {
-			trackingIds: ['G-MFJ3E8N4SC'],
-			pluginConfig: {
-				head: true,
-			},
-		},
-	},
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ['G-MFJ3E8N4SC'],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -34,24 +34,24 @@ module.exports = {
     {
       resolve: "gatsby-theme-primer-wiki",
       options: {
-        nav: [ 
+        nav: [
           {
             title: "Github",
             url: "https://github.com/Cyma-s/",
           },
         ],
-	icon : "./static/logo.png",
+        icon: "./static/logo.png",
         editUrl: "https://github.com/Cyma-s/Cyma-s.github.io/tree/main/content/",
         editUrlText: "수정하기",
-	defaultColorMode: "auto",
-	shouldShowLatestOnIndex: true,
-	shouldSupportLatest: true,
-	defaultIndexLatestPostCount: 20,
-	sidebarComponents: ["latest", "tag"],
-	shouldShowTagGroupsOnIndex: true,
-	shouldSupportTags: true,
-	tagText: "TAGS",
- 	lastUpdatedText: "최근 수정 시간",
+        defaultColorMode: "auto",
+        shouldShowLatestOnIndex: true,
+        shouldSupportLatest: true,
+        defaultIndexLatestPostCount: 20,
+        sidebarComponents: ["latest", "tag"],
+        shouldShowTagGroupsOnIndex: true,
+        shouldSupportTags: true,
+        tagText: "TAGS",
+        lastUpdatedText: "최근 수정 시간",
       },
     },
     {
@@ -60,7 +60,7 @@ module.exports = {
         name: siteMetadata.title,
         short_name: siteMetadata.shortName,
         start_url: pathPrefix,
-	lang: `ko`,
+        lang: `ko`,
         background_color: `#f7f0eb`,
         display: `standalone`,
         icon: path.resolve(__dirname, "./static/logo.png"),
@@ -74,7 +74,7 @@ module.exports = {
       options: {
         host: siteMetadata.siteUrl,
         sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
-        policy: [{ userAgent: "*", allow: "/"}],
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
@@ -83,11 +83,11 @@ module.exports = {
         types: ["Mdx"], // or ["MarkdownRemark"] (or both)
       },
     },
-	`gatsby-transformer-remark`,
-	{
-		resolve: `gatsby-plugin-feed`,
-		options: {
-      query: `
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
         {
           site {
             siteMetadata {
@@ -99,44 +99,46 @@ module.exports = {
           }
         }
       `,
-      feeds: [
-        {
-          serialize: ({ query: { site, allMarkdownRemark } }) => {
-            return allMarkdownRemark.edges.map(edge => {
-              return Object.assign({}, edge.node.frontmatter, {
-                description: edge.node.excerpt,
-                date: edge.node.frontmatter.date,
-                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                custom_elements: [{ 'content:encoded': edge.node.html }],
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                })
               })
-            })
-          },
-          query: `
+            },
+            query: `
             {
               allMarkdownRemark(
-                sort: { fields: [frontmatter___date], order: DESC }
-                filter: { frontmatter: { draft: { eq: false } } },
+                sort: {fields: frontmatter___date, order: DESC}
+                filter: {fields: {slug: {nin: ["/", "/placeholder/"]}}}
               ) {
                 edges {
                   node {
-                    excerpt
-                    html
-                    fields { slug }
+                    fields {
+                      slug
+                    }
                     frontmatter {
                       title
                       date
+                      updated
                     }
+                    excerpt
                   }
                 }
               }
             }
-          `,
-          output: '/rss.xml',
-		  title: "Vero Wiki RSS Feed",
-        },
-      ],
-    },	 
-	},
+            `,
+            output: '/rss.xml',
+            title: "Vero Wiki RSS Feed",
+          },
+        ],
+      },
+    },
   ],
 };
