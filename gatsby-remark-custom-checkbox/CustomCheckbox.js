e6b0a67efc7210a6ed4695a7c9e1bc525cd8334f
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-10-05 11:57:23 +0900

    글 자동 업로드: 2023-10-05 11:57:23
    Affected files:
    contents/.obsidian/plugins/recent-files-obsidian/data.json
    contents/.obsidian/workspace.json
    contents/posts/2023-10-04.md
    gatsby-remark-custom-checkbox/CustomCheckbox.js
    gatsby-remark-custom-checkbox/checkbox.module.css
    gatsby-remark-custom-checkbox/index.js

--- gatsby-remark-custom-checkbox/CustomCheckbox.js
+++ gatsby-remark-custom-checkbox/CustomCheckbox.js
@@ -4,34 +4,46 @@ import styles from './Checkbox.module.css';
 import ReactIcon from "react-icons/fa";
 
 const ICON_MAPPINGS = {
-    '>': ReactIcon.FaShare,
-    '<': ReactIcon.FaCalendar,
-    '!': ReactIcon.FaExclamation,
-    '-': ReactIcon.FaTimes,
-    '/': ReactIcon.FaStarHalfAlt,
-    '?': ReactIcon.FaQuestion,
-    '1': ReactIcon.FaComment,
-    '2': ReactIcon.FaComment,
-    '3': ReactIcon.FaComment,
-    '4': ReactIcon.FaComment,
-    '5': ReactIcon.FaComment,
-    '6': ReactIcon.FaComment,
-    '7': ReactIcon.FaComment,
-    '8': ReactIcon.FaComment,
-    '9': ReactIcon.FaComment,
-    '0': ReactIcon.FaComment,
+    '>': <ReactIcon.FaShare/>,
+    '<': <ReactIcon.FaCalendar/>,
+    '!': <ReactIcon.FaExclamation/>,
+    '-': <ReactIcon.FaTimes/>,
+    '/': <ReactIcon.FaStarHalfAlt/>,
+    '?': <ReactIcon.FaQuestion/>,
+    '*': <ReactIcon.FaStar/>,
+    'n': <ReactIcon.FaThumbtack/>,
+    'l': <ReactIcon.FaMapMarkerAlt/>,
+    'i': <ReactIcon.FaInfoCircle/>,
+    'I': <ReactIcon.FaLightbulb/>,
+    'S': <ReactIcon.FaDollarSign/>,
+    'p': <ReactIcon.FaThumbsUp/>,
+    'c': <ReactIcon.FaThumbsDown/>,
+    'b': <ReactIcon.FaBookmark/>,
+    '\"': <ReactIcon.FaQuoteLeft/>,
+    '1': <ReactIcon.FaComment/>,
+    '2': <ReactIcon.FaComment/>,
+    '3': <ReactIcon.FaComment/>,
+    '4': <ReactIcon.FaComment/>,
+    '5': <ReactIcon.FaComment/>,
+    '6': <ReactIcon.FaComment/>,
+    '7': <ReactIcon.FaComment/>,
+    '8': <ReactIcon.FaComment/>,
+    '9': <ReactIcon.FaComment/>,
+    '0': <ReactIcon.FaComment/>,
   };
   
 
 
-const CustomCheckbox = ({ iconKey, ...props }) => {
-  const iconValue = ICON_MAPPINGS[iconKey] || iconKey; // 딕셔너리에 없는 경우에 대비하여 기본값 설정
-  return (
-    <label className={styles.checkboxContainer}>
-      <input type="checkbox" className={styles.checkbox} {...props} />
-      <span className={styles.checkboxSymbol} data-icon={iconValue}></span>
-    </label>
-  );
-}
-
-export default CustomCheckbox;
+  const CustomCheckbox = ({ iconKey, ...props }) => {
+    const iconComponent = ICON_MAPPINGS[iconKey] || null; // 딕셔너리에 없는 경우에 대비하여 기본값 설정
+    return (
+      <label className={styles.checkboxContainer}>
+        <input type="checkbox" className={styles.checkbox} {...props} />
+        <span className={styles.checkboxSymbol}>
+          {iconComponent}
+        </span>
+      </label>
+    );
+  }
+  
+  export default CustomCheckbox;
--- gatsby-remark-custom-checkbox/index.js
+++ gatsby-remark-custom-checkbox/index.js
@@ -2,7 +2,7 @@ const visit = require("unist-util-visit");
 
 module.exports = ({ markdownAST }, pluginOptions) => {
   visit(markdownAST, "text", node => {
-    const regex = /\[(>|\[|<|\]|\!|-|\/|\?|\*|n|l|i|I|S|p|c|b|"|0|1|2|3|4|5|6|7|8|9)\]/g;
+    const regex = /^-\s\[(>|\[|<|\]|\!|-|\/|\?|\*|n|l|i|I|S|p|c|b|"|0|1|2|3|4|5|6|7|8|9)\]/gm;
     const matches = node.value.match(regex);
 
     if (matches) {
