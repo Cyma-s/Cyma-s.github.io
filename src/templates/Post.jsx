e0b123ff295fb80e6738e0334e667c9d731e7f49
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2025-02-25 19:43:02 +0100

    config: 템플릿에서 read time 제거

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -13,7 +13,7 @@ const Post = ({ data }) => {
 
   const { title, date, updated, tags, series } = post.frontmatter
   const { excerpt } = post
-  const { readingTime, slug } = post.fields
+  const { slug } = post.fields
 
   let filteredSeries = []
   if (series !== null) {
@@ -41,7 +41,6 @@ const Post = ({ data }) => {
           date={date}
           updated={updated}
           tags={tags}
-          minToRead={Math.round(readingTime.minutes)}
         />
         {filteredSeries.length > 0 && (
           <Article.Series header={series} series={filteredSeries} />
@@ -80,9 +79,6 @@ export const pageQuery = graphql`
       }
       fields {
         slug
-        readingTime {
-          minutes
-        }
       }
     }
     seriesList: allMarkdownRemark(
