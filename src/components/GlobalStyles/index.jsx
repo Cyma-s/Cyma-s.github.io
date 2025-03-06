c5c4fd0f241697993fba2b291be20881f85e0a43
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2025-03-06 22:43:43 +0100

    config: 폰트 변경 & content: 3/6 TIL

--- src/components/GlobalStyles/index.jsx
+++ src/components/GlobalStyles/index.jsx
@@ -1,13 +1,27 @@
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
 
+  @font-face {
+    font-family: "Eulyoo1945";
+    src: url("/fonts/Eulyoo1945-Regular.otf") format("opentype");
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Eulyoo1945";
+    src: url("/fonts/Eulyoo1945-SemiBold.otf") format("opentype");
+    font-weight: 700;
+    font-style: bold;
+  }
+
   body {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: "Eulyoo1945", sans-serif;
     background: ${props => props.theme.colors.bodyBackground};
   }
-`
+`;
 
-export default GlobalStyles
+export default GlobalStyles;
